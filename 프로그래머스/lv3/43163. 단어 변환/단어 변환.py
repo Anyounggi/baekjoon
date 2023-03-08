693fea581492c862894b84056adb05a7d8633f3f
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2023-03-08 22:14:47 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,23 +1,25 @@
 from collections import deque
 
+
 def solution(begin, target, words):
-    if target not in words:
+    if target not in words: # target이 리스트에 없으면 오류
         return 0
-    
+
     Q = deque()
     Q.append((begin, 0))
-    
+
     while Q:
         x, answer = Q.popleft()
-        if x == target:
-            return answer
-        
+
         for i in range(len(words)):
             cnt = 0
             for j in range(len(x)):
                 if x[j] != words[i][j]:
                     cnt += 1
             if cnt == 1:
-                Q.append((words[i], answer+1))
-    
-    return 0
+                if words[i] == target:
+                    return answer+1
+                else:
+                    Q.append((words[i], answer + 1))
+
+    return 0    # target이 리스트에는 있었지만 변경 실패면 오류
