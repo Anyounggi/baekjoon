53af92deda49e43c34d95838eb6209d874ed54e5
Anyounggi <122584209+Anyounggi@users.noreply.github.com>
2023-03-11 22:14:16 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,9 +1,9 @@
 def solution(n):
     answer = 0
-    a = bin(n)[2:].count('1')
+    a = bin(n).count('1')
     while True:
         n += 1
-        if bin(n)[2:].count('1') == a:
+        if bin(n).count('1') == a:
             answer = n
             break
     return answer
